dbe1390c5cc97baffb643a1148681bcb9f5356e8
redkuul <l.vandervelde@outlook.com>
2021-06-23 12:27:50 +0200

    increased probing speed

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1245,7 +1245,7 @@
 #define XY_PROBE_FEEDRATE (133*60)
 
 // Feedrate (mm/min) for the first approach when double-probing (MULTIPLE_PROBING == 2)
-#define Z_PROBE_FEEDRATE_FAST (4*60)
+#define Z_PROBE_FEEDRATE_FAST (50*60) //Luuk: was 100*60 in old config
 
 // Feedrate (mm/min) for the "accurate" probe of each point
 #define Z_PROBE_FEEDRATE_SLOW (Z_PROBE_FEEDRATE_FAST / 2)
